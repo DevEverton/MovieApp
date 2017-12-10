8674bc00b7f3b339df15dea0c324c4c6b84e4303
DevEverton <everton.mercante@gmail.com>
2017-12-10 14:47:37 -0300

    Refactor

--- MovieApp/ViewController.swift
+++ MovieApp/ViewController.swift
@@ -9,11 +9,11 @@
 import UIKit
 
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UICollectionViewDataSource, UICollectionViewDelegate {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     var categories = ["Action", "Drama", "Science/Fiction", "Kids", "Horror"]
     
-    static var movies = ["Action":[#imageLiteral(resourceName: "action1"), #imageLiteral(resourceName: "action2"), #imageLiteral(resourceName: "action3"), #imageLiteral(resourceName: "action4"), #imageLiteral(resourceName: "action5"), #imageLiteral(resourceName: "action6")], "Drama":[#imageLiteral(resourceName: "drama1"), #imageLiteral(resourceName: "drama2"), #imageLiteral(resourceName: "drama3"), #imageLiteral(resourceName: "drama4"), #imageLiteral(resourceName: "drama5"), #imageLiteral(resourceName: "drama6")], "Science/Fiction":[#imageLiteral(resourceName: "science1"), #imageLiteral(resourceName: "science2"), #imageLiteral(resourceName: "science3"), #imageLiteral(resourceName: "science4"), #imageLiteral(resourceName: "science5"), #imageLiteral(resourceName: "science6")], "Kids":[#imageLiteral(resourceName: "kids1"), #imageLiteral(resourceName: "kids2"), #imageLiteral(resourceName: "kids3"), #imageLiteral(resourceName: "kids4"), #imageLiteral(resourceName: "kids5"), #imageLiteral(resourceName: "kids6")], "Horror":[#imageLiteral(resourceName: "horror1"), #imageLiteral(resourceName: "horror2"), #imageLiteral(resourceName: "horror3"), #imageLiteral(resourceName: "horror4"), #imageLiteral(resourceName: "horror5"), #imageLiteral(resourceName: "horror6")] ]
+    var movies = ["Action":[#imageLiteral(resourceName: "action1"), #imageLiteral(resourceName: "action2"), #imageLiteral(resourceName: "action3"), #imageLiteral(resourceName: "action4"), #imageLiteral(resourceName: "action5"), #imageLiteral(resourceName: "action6")], "Drama":[#imageLiteral(resourceName: "drama1"), #imageLiteral(resourceName: "drama2"), #imageLiteral(resourceName: "drama3"), #imageLiteral(resourceName: "drama4"), #imageLiteral(resourceName: "drama5"), #imageLiteral(resourceName: "drama6")], "Science/Fiction":[#imageLiteral(resourceName: "science1"), #imageLiteral(resourceName: "science2"), #imageLiteral(resourceName: "science3"), #imageLiteral(resourceName: "science4"), #imageLiteral(resourceName: "science5"), #imageLiteral(resourceName: "science6")], "Kids":[#imageLiteral(resourceName: "kids1"), #imageLiteral(resourceName: "kids2"), #imageLiteral(resourceName: "kids3"), #imageLiteral(resourceName: "kids4"), #imageLiteral(resourceName: "kids5"), #imageLiteral(resourceName: "kids6")], "Horror":[#imageLiteral(resourceName: "horror1"), #imageLiteral(resourceName: "horror2"), #imageLiteral(resourceName: "horror3"), #imageLiteral(resourceName: "horror4"), #imageLiteral(resourceName: "horror5"), #imageLiteral(resourceName: "horror6")] ]
 
     
     override func viewDidLoad() {
@@ -56,26 +56,30 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
     }
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return ViewController.movies["Action"]!.count
+}
 
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies["Action"]!.count
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let videoCell = collectionView.dequeueReusableCell(withReuseIdentifier: "videoCell", for: indexPath) as! VideoCell
         
         switch collectionView.tag {
-        case 0: videoCell.videoImage.image = ViewController.movies["Action"]?[indexPath.row]
-        case 1: videoCell.videoImage.image = ViewController.movies["Drama"]?[indexPath.row]
-        case 2: videoCell.videoImage.image = ViewController.movies["Science/Fiction"]?[indexPath.row]
-        case 3: videoCell.videoImage.image = ViewController.movies["Kids"]?[indexPath.row]
-        case 4: videoCell.videoImage.image = ViewController.movies["Horror"]?[indexPath.row]
+        case 0: videoCell.videoImage.image = movies["Action"]?[indexPath.row]
+        case 1: videoCell.videoImage.image = movies["Drama"]?[indexPath.row]
+        case 2: videoCell.videoImage.image = movies["Science/Fiction"]?[indexPath.row]
+        case 3: videoCell.videoImage.image = movies["Kids"]?[indexPath.row]
+        case 4: videoCell.videoImage.image = movies["Horror"]?[indexPath.row]
         default: break
         }
-
+        
         return videoCell
     }
-
+  
 }
 
 
